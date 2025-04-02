ab03a9d0cfd9e248bf7807c17e9ab598f3bc64a9
Flowerhub-Kanei <jilavut21@gmail.com>
2025-04-03 06:57:17 +0700

    Update AnimePower.lua

--- AnimePower.lua
+++ AnimePower.lua
@@ -2,13 +2,11 @@ local lib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodbal
 local win = lib:CreateWindow("Floral Hub V3")
 local fld = win:CreateFolder("Main")
 
--- ฟังก์ชันโจมตี
 local function atk()
     local args = { [1] = "attack" }
     game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("events"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
 end
 
-local atkTgl = false
 fld:Toggle("Auto Attack (Click)", function(state)
     atkTgl = state
     print("Auto Attack:", atkTgl)
@@ -22,9 +20,6 @@ end)
 
 fld:Label("Auto Farm", { TextSize = 25, TextColor = Color3.fromRGB(255,255,255), BgColor = Color3.fromRGB(69,69,69) })
 
-fld:Button("Button", function() print("Elym Winning") end)
-
--- ค่าต่างๆที่ใช้
 local rs = game:GetService("ReplicatedStorage")
 local enf = rs:WaitForChild("Assets"):WaitForChild("Enemies")
 local py = game:GetService("Players").LocalPlayer
@@ -32,9 +27,6 @@ local char = py.Character or py.CharacterAdded:Wait()
 local hrp = char:WaitForChild("HumanoidRootPart")
 
 local enList = {}
-local selEn = nil
-local autoTP = false
-
 
 local function updEn()
     enList = {}
@@ -44,10 +36,11 @@ local function updEn()
     enDrop:Refresh(enList)
 end
 
-updEn()
+updEn() ----อันนี้กุขก.เลยใช้gpt
 enf.ChildAdded:Connect(updEn)
 enf.ChildRemoved:Connect(updEn)
 
+local selEn = nil
 fld:Dropdown("Select Mob", enList, true, function(mob)
     selEn = mob
     print("Selected Mob:", mob)
@@ -58,7 +51,6 @@ local function killmontp()
         for _, en in pairs(enf:GetChildren()) do
             if en.Name == selEn and en.PrimaryPart then
                 hrp.CFrame = en.PrimaryPart.CFrame + Vector3.new(0, 5, 0)
-                print("Teleported to:", en.Name)
                 return
             end
         end
